eb9a426cc0c05f358c2ecadddabb297e8748c926
sean <sean.nguyen.goldenowl@gmail.com>
2025-11-07 23:36:36 +0700

    feat: evalueate model

--- model.py
+++ model.py
@@ -4,19 +4,18 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor  
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
 import math
 
-# ===============================
-# 1️⃣ Load Cleaned Dataset
-# ===============================
+
+# 1 Load Cleaned Dataset
+
 df = pd.read_csv("clean_data_set/cleaned_stock_data.csv")
 
-# Drop NaN caused by pct_change()
-df = df.dropna()
 
 # Chọn features và target
 X = df[['Open', 'High', 'Low', 'Volume', 'Return']]
@@ -25,24 +24,29 @@ y = df['Close']
 # Chia train/test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
 
-
-# Define Models
+# 2️ Define Models
 
 models = {
-    "LinearRegression": LinearRegression(),
     "RandomForest": RandomForestRegressor(n_estimators=100, random_state=42),
     "SVR": SVR(kernel='rbf'),
-    "DecisionTree": DecisionTreeRegressor(random_state=42) 
+    "DecisionTree": DecisionTreeRegressor(random_state=42),
+    "XGBoost": XGBRegressor(
+        n_estimators=200,
+        learning_rate=0.05,
+        max_depth=5,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        random_state=42
+    )
 }
 
-# ===============================
-# 3️⃣ Create result folder
-# ===============================
+# 3️ Create result folder
+
 os.makedirs("result", exist_ok=True)
 
-# ===============================
-# 4️⃣ Train & Evaluate Models
-# ===============================
+
+# 4️ Train & Evaluate Models
+
 results = []
 
 for name, model in models.items():
@@ -52,7 +56,7 @@ for name, model in models.items():
 
     # --- Metrics ---
     mae = mean_absolute_error(y_test, y_pred)
-    nmae = mae / np.mean(np.abs(y_test))  # normalized MAE
+    nmae = mae / np.mean(np.abs(y_test))
     rmse = math.sqrt(mean_squared_error(y_test, y_pred))
     r2 = r2_score(y_test, y_pred)
 
@@ -73,9 +77,9 @@ for name, model in models.items():
         "R2": r2
     })
 
-# ===============================
-# 5️⃣ Save Evaluation Table
-# ===============================
+
+# 5️ Save Evaluation Table
+
 eval_df = pd.DataFrame(results)
 eval_df.to_csv("result/evaluate_model.csv", index=False)
 
